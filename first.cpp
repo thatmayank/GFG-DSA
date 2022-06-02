d15033602b32ccc047a37bf53f6ef9ec2af983a4
Mayank <63830975+thatmayank@users.noreply.github.com>
2022-06-02 12:17:40 +0530

    GFG DSA QUESTIONS

---
+++ first.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    string name;
+    cout<<"Hello world\n";
+    cout<<"Enter your Name\n";
+    getline(cin, name);
+    cout<<"Hello "+name;
+    return 0;
+}
