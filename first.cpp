926cfe560a17121a4cf1b55c1f25004514a6ff8e
Mayank <63830975+thatmayank@users.noreply.github.com>
2022-06-03 17:18:25 +0530

    .

--- first.cpp
+++ first.cpp
@@ -1,10 +1,11 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main()
+{
     string name;
-    cout<<"Hello world\n";
-    cout<<"Enter your Name\n";
+    cout << "Hello world\n";
+    cout << "Enter your Name\n";
     getline(cin, name);
-    cout<<"Hello "+name;
+    cout << "Hello " + name;
     return 0;
 }
